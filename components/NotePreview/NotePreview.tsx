b856e6c20e719c059b75cc8bded9d328eb56deed
Mariana_k <m.koval000@gmail.com>
2025-08-07 21:59:37 +0300

    add btn

--- components/NotePreview/NotePreview.tsx
+++ components/NotePreview/NotePreview.tsx
@@ -1,11 +1,21 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import css from './NotePreview.module.css';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'next/navigation';
 import { fetchNoteById } from '@/lib/api';
-import css from './NotePreview.module.css';
 
-const NoteDetailsClient = () => {
+interface Props {
+  children?: React.ReactNode;
+}
+
+const NoteDetailsClient = ({ children }: Props) => {
+  const router = useRouter();
+  const handleBack = () => {
+    router.back();
+  };
+
   const { id } = useParams<{ id: string }>();
 
   const {
@@ -19,11 +29,14 @@ const NoteDetailsClient = () => {
   });
 
   if (isLoading) return <p>Loading, please wait...</p>;
-
   if (error || !note) return <p>Something went wrong.</p>;
 
   return (
     <div className={css.container}>
+      {children}
+      <button className={css.backBtn} onClick={handleBack}>
+        CLOSE
+      </button>
       <div className={css.item}>
         <div className={css.header}>
           <h2>{note.title}</h2>
