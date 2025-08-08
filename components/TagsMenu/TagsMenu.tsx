49e9e3ba50e535d6b3e8698fa8dea04a94132685
Mariana_k <m.koval000@gmail.com>
2025-08-08 09:25:13 +0300

    update

--- components/TagsMenu/TagsMenu.tsx
+++ components/TagsMenu/TagsMenu.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import css from './TagsMenu.module.css';
 import { useState } from 'react';
 
+
+
 const TagsMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
